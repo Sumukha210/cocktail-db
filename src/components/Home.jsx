195a887343ad8520029eda2b8bff86bf76827071
Sumukha210 <sumukhakb@gmail.com>
2020-11-22 02:00:15 +0530

    final board

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,11 @@
 import { Container, Grid, Typography } from "@material-ui/core";
-import React, { useContext } from "react";
+import React, { lazy, Suspense, useContext } from "react";
 import { DataContext } from "../Context/DataContext";
-import CustomCard from "./CustomCard";
+// import CustomCard from "./CustomCard";
 import SearchBar from "./SearchBar";
 import Spinner from "./Spinner";
+
+const CustomCard = lazy(() => import("./CustomCard"));
 
 const Home = () => {
   const [state] = useContext(DataContext);
@@ -18,42 +20,44 @@ const Home = () => {
           Cocktails
         </Typography>
 
-        <Grid container spacing={2}>
-          {state.alldata ? (
-            state.alldata.map(
-              ({
-                idDrink,
-                strDrink,
-                strGlass,
-                strAlcoholic,
-                strDrinkThumb,
-              }) => (
-                <CustomCard
-                  key={idDrink}
-                  title={strDrink}
-                  sub__title={strGlass}
-                  type={strAlcoholic}
-                  img={strDrinkThumb}
-                  id={idDrink}
-                />
+        <Suspense fallback={<Spinner />}>
+          <Grid container spacing={2}>
+            {state.alldata ? (
+              state.alldata.map(
+                ({
+                  idDrink,
+                  strDrink,
+                  strGlass,
+                  strAlcoholic,
+                  strDrinkThumb,
+                }) => (
+                  <CustomCard
+                    key={idDrink}
+                    title={strDrink}
+                    sub__title={strGlass}
+                    type={strAlcoholic}
+                    img={strDrinkThumb}
+                    id={idDrink}
+                  />
+                )
               )
-            )
-          ) : (
-            <h1>No data to show</h1>
-          )}
-          {state.error && (
-            <h1
-              style={{
-                marginTop: "2rem",
-                textAlign: "center",
-                fontWeight: "700",
-              }}>
-              {state.error}......
-            </h1>
-          )}
+            ) : (
+              <h1>No data to show</h1>
+            )}
+            {state.error && (
+              <h1
+                style={{
+                  marginTop: "2rem",
+                  textAlign: "center",
+                  fontWeight: "700",
+                }}>
+                {state.error}......
+              </h1>
+            )}
 
-          {state.loading && <Spinner />}
-        </Grid>
+            {state.loading && <Spinner />}
+          </Grid>
+        </Suspense>
       </Container>
     </>
   );
